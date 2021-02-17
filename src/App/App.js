55eb32e7ac7d031e38f1c97d2f7d1b3673307128
Elsa Fluss <elsafluss@gmail.com>
2021-02-17 09:24:30 -0700

    Show API data on page load.

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => response.json())
+    .then(response => this.setState({ reservations: response }))
+    .catch(error => console.log(error))
+  }
+
   render() {
+    const showReservations = this.state.reservations.map(item => 
+      <>
+        <p key={`${item.id}${item.name}`}>{item.name}</p>
+        <p key={`${item.id}${item.date}`}>{item.date}</p>
+        <p key={`${item.id}${item.number}`}>{item.number}</p>
+        <p key={`${item.id}${item.time}`}>{item.time}</p>
+      </>
+    )
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
@@ -10,7 +34,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          {showReservations}
         </div>
       </div>
     )
